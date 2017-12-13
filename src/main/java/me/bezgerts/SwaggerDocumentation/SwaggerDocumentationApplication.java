1747cd83db7648e696ce7e0aa86e85ea8fe61db9
Bezgerts Arkdaiy <bezgerts@gmail.com>
2017-12-13 16:54:55 +0300

    Initial commit

---
+++ src/main/java/me/bezgerts/SwaggerDocumentation/SwaggerDocumentationApplication.java
@@ -0,0 +1,12 @@
+package me.bezgerts.SwaggerDocumentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerDocumentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerDocumentationApplication.class, args);
+	}
+}
---
+++ src/test/java/me/bezgerts/SwaggerDocumentation/SwaggerDocumentationApplicationTests.java
@@ -0,0 +1,16 @@
+package me.bezgerts.SwaggerDocumentation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwaggerDocumentationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
