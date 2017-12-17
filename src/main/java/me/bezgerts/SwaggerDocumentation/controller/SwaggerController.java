624b77a3eb0cc9149436d9779d4749695b575d5b
Аркадий Безгерц <bezgerts@gmail.com>
2017-12-17 20:49:46 +0300

    Добавление нового сервиса и нового контроллера.

--- src/main/java/me/bezgerts/SwaggerDocumentation/controller/SwaggerController.java
+++ src/main/java/me/bezgerts/SwaggerDocumentation/controller/SwaggerController.java
@@ -2,7 +2,10 @@ package me.bezgerts.SwaggerDocumentation.controller;
 
 import me.bezgerts.SwaggerDocumentation.service.swagger.SwaggerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SwaggerController {
@@ -10,6 +13,9 @@ public class SwaggerController {
     @Autowired
     private SwaggerService swaggerService;
 
-
+    @RequestMapping(path = "/paths")
+    public List<String> getPaths() {
+        return swaggerService.getPaths();
+    }
 
 }
--- src/main/java/me/bezgerts/SwaggerDocumentation/service/swagger/SwaggerServiceImpl.java
+++ src/main/java/me/bezgerts/SwaggerDocumentation/service/swagger/SwaggerServiceImpl.java
@@ -1,8 +1,12 @@
 package me.bezgerts.SwaggerDocumentation.service.swagger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import v2.io.swagger.models.Swagger;
+import v2.io.swagger.parser.SwaggerParser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.codehaus.groovy.tools.shell.util.Logger.io;
@@ -11,13 +15,12 @@ import static org.codehaus.groovy.tools.shell.util.Logger.io;
 @Transactional
 public class SwaggerServiceImpl implements SwaggerService {
 
-
-
-    public static void main(){
-
+    private Swagger getSwagger() {
+        Swagger swagger = new SwaggerParser().read("swagger.json");
+        return swagger;
     }
 
     public List<String> getPaths() {
-        return null;
+        return new ArrayList<>(getSwagger().getPaths().keySet());
     }
 }
