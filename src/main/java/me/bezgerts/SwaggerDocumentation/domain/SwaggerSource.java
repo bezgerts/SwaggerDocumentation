2c0547a41f500a514515f9186d6365999f8d7284
Аркадий Безгерц <bezgerts@gmail.com>
2017-12-19 13:18:23 +0300

    Добавлена возможность указывать в application.properties источник Swagger (исправление предыдущего коммита)

--- src/main/java/me/bezgerts/SwaggerDocumentation/domain/SwaggerSource.java
+++ src/main/java/me/bezgerts/SwaggerDocumentation/domain/SwaggerSource.java
@@ -1,7 +1,6 @@
 package me.bezgerts.SwaggerDocumentation.domain;
 
-/**
- * Created by bezgerts on 19.12.17.
- */
 public enum SwaggerSource {
+    HTTP,
+    FILE
 }
--- src/main/java/me/bezgerts/SwaggerDocumentation/service/swagger/SwaggerServiceImpl.java
+++ src/main/java/me/bezgerts/SwaggerDocumentation/service/swagger/SwaggerServiceImpl.java
@@ -1,5 +1,8 @@
 package me.bezgerts.SwaggerDocumentation.service.swagger;
 
+import me.bezgerts.SwaggerDocumentation.domain.SwaggerSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import v2.io.swagger.models.Model;
@@ -13,8 +16,23 @@ import java.util.Map;
 @Transactional
 public class SwaggerServiceImpl implements SwaggerService {
 
+    @Autowired
+    private Environment env;
+
     private Swagger getSwagger() {
-        Swagger swagger = new SwaggerParser().read("swagger.json");
+
+        SwaggerSource swaggerSource = SwaggerSource.valueOf(env.getProperty("swagger.source"));
+        Swagger swagger = null;
+
+        switch (swaggerSource) {
+            case FILE:
+                swagger = new SwaggerParser().read(env.getProperty("swagger.source.file.name"));
+                break;
+
+            case HTTP:
+                swagger = new SwaggerParser().read(env.getProperty("swagger.source.http.url"));
+                break;
+        }
         return swagger;
     }
 
